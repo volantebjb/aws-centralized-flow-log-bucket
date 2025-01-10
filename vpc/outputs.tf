127ca21ef99d3177474671853f5041b9276e18b1
volantebjb <volantebjb@gmail.om>
2025-01-10 10:05:17 +0800

    Refactor vpc module

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,8 +2,3 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.vpc.id
 }
-
-output "vpc_cidr_block" {
-  description = "The CIDR block of the VPC"
-  value       = aws_vpc.vpc.cidr_block
-}
