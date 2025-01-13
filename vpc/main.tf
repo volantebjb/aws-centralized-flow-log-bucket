e995a297d941c2b6aca074d39fdc120604545ac7
volantebjb <volantebjb@gmail.om>
2025-01-13 08:51:29 +0800

    Add igw

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,12 +2,26 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
-    Name = var.vpc_name
-  }
 }
 
 resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = var.subnet_cidr_block
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.public.id
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -4,5 +4,6 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  value = aws_subnet.subnet.id
+  description = "The ID of the subnet"
+  value       = aws_subnet.subnet.id
 }
