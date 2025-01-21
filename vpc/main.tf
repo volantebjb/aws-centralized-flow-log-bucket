7b1a4d7fc4949046b82950c923fcea915936dd1a
volantebjb <volantebjb@gmail.om>
2025-01-21 21:45:58 +0800

    Add name variable in vpc module

--- vpc/main.tf
+++ vpc/main.tf
@@ -2,6 +2,9 @@ resource "aws_vpc" "vpc" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
+  tags = {
+    "Name" = "${var.vpc_name}-vpc"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
